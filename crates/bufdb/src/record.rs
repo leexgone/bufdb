b63ecafc217ab16d432bb2529318867e1624bcb7
ljz <ljz@jiuqi.com.cn>
2022-12-11 12:17:45 +0800

    isnull

--- crates/bufdb/src/record.rs
+++ crates/bufdb/src/record.rs
@@ -1,5 +1,9 @@
-#[derive(Debug, Clone)]
-enum DataValue {
+use crate::error::ErrorKind;
+use crate::error::Result;
+
+#[derive(Debug, Clone, PartialEq, Default)]
+enum Value {
+    #[default]
     NULL,
     STRING(String),
     DOUBLE(f64),
@@ -12,5 +16,37 @@ enum DataValue {
 
 #[derive(Debug, Clone)]
 pub struct Record {
-    values: Vec<DataValue>
+    values: Vec<Value>
+}
+
+impl Record {
+    pub fn new(len: usize) -> Record {
+        let mut values = Vec::with_capacity(len);
+        for _ in 0..len {
+            values.push(Value::default());
+        }
+
+        Record { values }    
+    }
+
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn is_null(&self, index: usize) -> Result<bool> {
+        if let Some(val) = self.values.get(index) {
+            Ok(val == &Value::NULL)
+        } else {
+            Err(ErrorKind::OutOfBounds.into())
+        }
+    }
+
+    pub fn set_null(&mut self, index: usize) -> Result<()> {
+        if let Some(val) = self.values.get_mut(index) {
+            *val = Value::NULL;
+            Ok(())
+        } else {
+            Err(ErrorKind::OutOfBounds.into())
+        }
+    }
 }
