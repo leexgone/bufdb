93e6e4f986d9241beed0648df0e4ff54697ad544
ljz <ljz@jiuqi.com.cn>
2022-12-14 17:56:23 +0800

    dev record

--- crates/bufdb/src/record.rs
+++ crates/bufdb/src/record.rs
@@ -1,6 +1,9 @@
 use crate::error::ErrorKind;
 use crate::error::Result;
 
+/// Defines `TimeStamp` type to store datetime.
+pub type TimeStamp = u64;
+
 #[derive(Debug, Clone, PartialEq, Default)]
 enum Value {
     #[default]
@@ -9,7 +12,7 @@ enum Value {
     DOUBLE(f64),
     INTEGER(i32),
     LONG(i64),
-    DATETIME(u64),
+    DATETIME(TimeStamp),
     BOOLEAN(bool),
     BLOB(Box<Vec<u8>>)
 }
@@ -17,6 +20,54 @@ enum Value {
 impl Value {
     fn is_null(&self) -> bool {
         self == &Value::NULL
+    }
+}
+
+impl From<&str> for Value {
+    fn from(val: &str) -> Self {
+        Self::STRING(Box::new(val.into()))
+    }
+}
+
+impl From<String> for Value {
+    fn from(val: String) -> Self {
+        Self::STRING(Box::new(val))
+    }
+}
+
+impl From<f64> for Value {
+    fn from(val: f64) -> Self {
+        Self::DOUBLE(val)
+    }
+}
+
+impl From<i32> for Value {
+    fn from(val: i32) -> Self {
+        Self::INTEGER(val)
+    }
+}
+
+impl From<i64> for Value {
+    fn from(val: i64) -> Self {
+        Self::LONG(val)
+    }
+}
+
+impl From<TimeStamp> for Value {
+    fn from(val: TimeStamp) -> Self {
+        Self::DATETIME(val)
+    }
+}
+
+impl From<bool> for Value {
+    fn from(val: bool) -> Self {
+        Self::BOOLEAN(val)
+    }
+}
+
+impl From<Vec<u8>> for Value {
+    fn from(val: Vec<u8>) -> Self {
+        Self::BLOB(Box::new(val))
     }
 }
 
@@ -39,23 +90,6 @@ impl Record {
         self.values.len()
     }
 
-    pub fn is_null(&self, index: usize) -> Result<bool> {
-        if let Some(val) = self.values.get(index) {
-            Ok(val.is_null())
-        } else {
-            Err(ErrorKind::OutOfBounds.into())
-        }
-    }
-
-    pub fn set_null(&mut self, index: usize) -> Result<()> {
-        if let Some(val) = self.values.get_mut(index) {
-            *val = Value::NULL;
-            Ok(())
-        } else {
-            Err(ErrorKind::OutOfBounds.into())
-        }
-    }
-
     fn get(&self, index: usize) -> Result<&Value> {
         if let Some(val) = self.values.get(index) {
             Ok(val)
@@ -72,6 +106,23 @@ impl Record {
         }
     }
 
+    pub fn is_null(&self, index: usize) -> Result<bool> {
+        let val = self.get(index)?;
+        Ok(val.is_null())
+    }
+
+    pub fn set_null(&mut self, index: usize) -> Result<()> {
+        let val = self.get_mut(index)?;
+        *val = Value::NULL;
+        Ok(())
+    }
+
+    fn set_value<T>(&mut self, index: usize, v: T) -> Result<()> where T: Into<Value> {
+        let val = self.get_mut(index)?;
+        *val = v.into();
+        Ok(())
+    }
+
     pub fn get_str(&self, index: usize) -> Result<Option<&str>> {
          let val = self.get(index)?;
         match val {
@@ -82,9 +133,11 @@ impl Record {
     }
 
     pub fn set_str(&mut self, index: usize, str: &str) -> Result<()> {
-        let val = self.get_mut(index)?;
-        *val = Value::STRING(Box::new(str.into()));
-        Ok(())
+        self.set_value(index, str)
+    }
+
+    pub fn set_string(&mut self, index: usize, s: String) -> Result<()> {
+        self.set_value(index, s)
     }
 
     pub fn get_double(&self, index: usize) -> Result<Option<f64>> {
@@ -97,51 +150,59 @@ impl Record {
     }
 
     pub fn set_double(&mut self, index: usize, v: f64) -> Result<()> {
-        let val = self.get_mut(index)?;
-        *val = Value::DOUBLE(v);
-        Ok(())
+        self.set_value(index, v)
     }
 
     pub fn get_int(&self, index: usize) -> Result<i32> {
-        if let Some(val) = self.values.get(index) {
-            match val {
-                Value::INTEGER(v) => Ok(*v),
-                Value::NULL => Err(ErrorKind::NullValue.into()),
-                _ => Err(ErrorKind::ErrorType.into())
-            }
-        } else {
-            Err(ErrorKind::OutOfBounds.into())
+        let val = self.get(index)?;
+        match val {
+            Value::INTEGER(v) => Ok(*v),
+            Value::NULL => Err(ErrorKind::NullValue.into()),
+            _ => Err(ErrorKind::ErrorType.into())
         }
     }
 
     pub fn set_int(&mut self, index: usize, v: i32) -> Result<()> {
-        if let Some(val) = self.values.get_mut(index) {
-            *val = Value::INTEGER(v);
-            Ok(())
-        } else {
-            Err(ErrorKind::OutOfBounds.into())
-        }
+        self.set_value(index, v)
     }
 
     pub fn get_long(&self, index: usize) -> Result<Option<i64>> {
-        if let Some(val) = self.values.get(index) {
-            match val {
-                Value::LONG(v) => Ok(Some(*v)),
-                Value::NULL => Ok(None),
-                _ => Err(ErrorKind::ErrorType.into())
-            }
-        } else {
-            Err(ErrorKind::OutOfBounds.into())
+        let val = self.get(index)?;
+        match val {
+            Value::LONG(v) => Ok(Some(*v)),
+            Value::NULL => Ok(None),
+            _ => Err(ErrorKind::ErrorType.into())
         }
     }
 
     pub fn set_long(&mut self, index: usize, v: i64) -> Result<()> {
-        if let Some(val) = self.values.get_mut(index) {
-            *val = Value::LONG(v);
-            Ok(())
-        } else {
-            Err(ErrorKind::OutOfBounds.into())
+        self.set_value(index, v)
+    }
+
+    pub fn get_datetime(&self, index: usize) -> Result<Option<TimeStamp>> {
+        let val = self.get(index)?;
+        match val {
+            Value::DATETIME(v) => Ok(Some(*v)),
+            Value::NULL => Ok(None),
+            _ => Err(ErrorKind::ErrorType.into())
         }
+    }
+
+    pub fn set_datetime(&mut self, index: usize, v: TimeStamp) -> Result<()> {
+        self.set_value(index, v)
+    }
+
+    pub fn get_bool(&self, index: usize) -> Result<Option<bool>> {
+        let val = self.get(index)?;
+        match val {
+            Value::BOOLEAN(v) => Ok(Some(*v)),
+            Value::NULL => Ok(None),
+            _ => Err(ErrorKind::ErrorType.into())
+        }
+    }
+
+    pub fn set_bool(&mut self, index: usize, v: bool) -> Result<()> {
+        self.set_value(index, v)
     }
 }
 
@@ -151,6 +212,6 @@ mod tests {
 
     #[test]    
     fn test_value_size() {
-        println!("Size of Value: {}", std::mem::size_of::<Value>())
+        println!("Size of Value: {}", std::mem::size_of::<Value>());
     }
 }
