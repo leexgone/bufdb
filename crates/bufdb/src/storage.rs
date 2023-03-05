f539c4c67e0b7241e6f8dfab9762f0743c8767d0
ljz <ljz@jiuqi.com.cn>
2023-03-05 12:18:47 +0800

    dev storage

--- crates/bufdb/src/storage.rs
+++ crates/bufdb/src/storage.rs
@@ -1,4 +1,6 @@
 use crate::error::Result;
+use crate::model::IndexDefine;
+use crate::model::TableDefine;
 
 #[derive(Debug, Default)]
 pub struct BufferEntry {
@@ -88,9 +90,57 @@ impl<const N: usize> From<[u8; N]> for BufferEntry  {
     }
 }
 
-pub trait Database {
+pub trait Database<C: Cursor> {
     fn count(&self) -> Result<usize>;
-    fn put(&self, key: &BufferEntry, data: &BufferEntry) -> Result<()>;
-    fn get(&mut self, key: &BufferEntry) -> Result<Option<BufferEntry>>;
+    fn put(&mut self, key: &BufferEntry, data: &BufferEntry) -> Result<()>;
+    fn get(&self, key: &BufferEntry) -> Result<Option<BufferEntry>>;
     fn delete(&mut self, key: &BufferEntry) -> Result<bool>;
+    fn open_cursor(&self) -> Result<C>;
+}
+
+pub trait Cursor {
+    fn search(&self, key: &BufferEntry, data: Option<&mut BufferEntry>) -> Result<bool>;
+    fn search_range(&self, key: &mut BufferEntry, data: Option<&mut BufferEntry>) -> Result<bool>;
+    fn next(&self, key: &mut BufferEntry, data: &mut BufferEntry) -> Result<bool>;
+    fn next_dup(&self, key: &mut BufferEntry, data: &mut BufferEntry) -> Result<bool>;
+    fn skip(&self, count: usize, key: Option<&mut BufferEntry>, data: Option<&mut BufferEntry>) -> Result<bool>;
+    fn delete(&mut self, key: &BufferEntry) -> Result<bool>;
+    fn update(&mut self, key: &BufferEntry, data: &BufferEntry) -> Result<bool>;
+}
+
+pub trait SecondaryCursor : Cursor {
+    fn s_search(&self, key: &BufferEntry, p_key: Option<&mut BufferEntry>, data: Option<&mut BufferEntry>) -> Result<bool>;
+    fn s_search_range(&self, key: &mut BufferEntry, p_key: Option<&mut BufferEntry>, data: Option<&mut BufferEntry>) -> Result<bool>;
+    fn s_next(&self, key: &mut BufferEntry, p_key: Option<&mut BufferEntry>, data: Option<&mut BufferEntry>) -> Result<bool>;
+    fn s_next_dup(&self, key: &mut BufferEntry, p_key: Option<&mut BufferEntry>, data: Option<&mut BufferEntry>) -> Result<bool>;
+}
+
+pub trait Environment {
+    type CURSOR: Cursor;
+    type SCUROSR: SecondaryCursor;
+    type DATABASE: Database<Self::CURSOR>;
+    type SDATABASE: Database<Self::SCUROSR>;
+
+    fn create_database<C: KeyComparator>(&mut self, define: TableDefine, config: TableConfig, comparator: C) -> Result<Self::DATABASE>;
+    fn create_secondary_database<C: KeyComparator>(&mut self, database: &Self::DATABASE, name: &str, define: IndexDefine, comparator: C) -> Result<Self::SDATABASE>;
+    fn drop_database(&mut self, name: &str) -> Result<()>;
+    fn drop_secondary_database(&mut self, name: &str) -> Result<()>;
+    fn truncate_database(&mut self, name: &str) -> Result<()>;
+    fn rename_database(&mut self, raw_name: &str, new_name: &str) -> Result<()>;
+}
+
+pub struct TableConfig {
+
+}
+
+pub trait KeyComparator {
+    fn compare(&self, key1: &BufferEntry, key2: &BufferEntry) -> Result<i32>;
+
+    fn find_shortest_separator<'a>(&self, start: &'a BufferEntry, _limit: &'a BufferEntry) -> Result<&'a BufferEntry> {
+        Ok(start)
+    }
+
+    fn fins_shortest_successor<'a>(&self, key: &'a BufferEntry) -> Result<&'a BufferEntry> {
+        Ok(key)
+    }
 }
