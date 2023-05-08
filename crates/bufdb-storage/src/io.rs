37fb6d72a84df1d1de75bae4e9bf672b904a8956
ljz <ljz@jiuqi.com.cn>
2023-05-08 18:18:08 +0800

    Input & Outpu

--- crates/bufdb-storage/src/io.rs
+++ crates/bufdb-storage/src/io.rs
@@ -16,8 +16,44 @@
 use std::io::Error;
 use std::io::ErrorKind;
 use std::io::Read;
+use std::io::Result;
 use std::io::Seek;
 use std::io::Write;
+
+/// `Input` trait 
+pub trait Input {
+    fn read_string(&self) -> Result<String>;
+    fn read_u8(&self) -> Result<u8>;
+    fn read_u16(&self) -> Result<u16>;
+    fn read_u32(&self) -> Result<u32>;
+    fn read_u64(&self) -> Result<u64>;
+    fn read_i8(&self) -> Result<i8>;
+    fn read_i16(&self) -> Result<i16>;
+    fn read_i32(&self) -> Result<i32>;
+    fn read_i64(&self) -> Result<i64>;
+    fn read_f64(&self) -> Result<f64>;
+    fn read_packed_i32(&self) -> Result<i32>;
+    fn read_packed_i64(&self) -> Result<i64>;
+}
+
+pub trait Output : Sized {
+    fn write_string(&mut self, s: &String) -> Result<()> {
+        self.write_str(s.as_str())
+    }
+
+    fn write_str(&mut self, s: &str) -> Result<()>;
+    fn write_u8(&mut self) -> Result<()>;
+    fn write_u16(&mut self) -> Result<()>;
+    fn write_u32(&mut self) -> Result<()>;
+    fn write_u64(&mut self) -> Result<()>;
+    fn write_i8(&mut self) -> Result<()>;
+    fn write_i16(&mut self) -> Result<()>;
+    fn write_i32(&mut self) -> Result<()>;
+    fn write_i64(&mut self) -> Result<()>;
+    fn write_f64(&mut self) -> Result<()>;
+    fn write_packed_i32(&mut self) -> Result<()>;
+    fn write_packed_i64(&mut self) -> Result<()>;
+}
 
 /// `BufferInput` is a reader for buffer entry.
 #[derive(Debug, Clone)]
@@ -89,7 +125,7 @@ impl <'a> BufferInput<'a> {
 }
 
 impl <'a> Read for BufferInput<'a> {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         if self.eof() {
             Err(Error::new(ErrorKind::UnexpectedEof, "read end of buffer"))
         } else {
@@ -109,7 +145,7 @@ impl <'a> Read for BufferInput<'a> {
 }
 
 impl <'a> Seek for BufferInput<'a> {
-    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> Result<u64> {
         let position: isize = match pos {
             std::io::SeekFrom::Start(p) => self.off as isize + p as isize,
             std::io::SeekFrom::End(p) => self.len as isize + p as isize,
@@ -124,12 +160,12 @@ impl <'a> Seek for BufferInput<'a> {
         }
     }
 
-    fn rewind(&mut self) -> std::io::Result<()> {
+    fn rewind(&mut self) -> Result<()> {
         self.pos = self.off;
         Ok(())
     }
 
-    fn stream_position(&mut self) -> std::io::Result<u64> {
+    fn stream_position(&mut self) -> Result<u64> {
         Ok(self.pos as _)
     }
 }
@@ -189,7 +225,7 @@ impl Default for BufferOutput {
 }
 
 impl Write for BufferOutput {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
         if buf.len() <= self.data.len() - self.pos {
             self.data[self.pos..self.pos + buf.len()].copy_from_slice(buf);
         } else {
@@ -202,13 +238,13 @@ impl Write for BufferOutput {
         Ok(buf.len())
     }
 
-    fn flush(&mut self) -> std::io::Result<()> {
+    fn flush(&mut self) -> Result<()> {
         Ok(())
     }
 }
 
 impl Seek for BufferOutput {
-    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> Result<u64> {
         let pos: isize = match pos {
             std::io::SeekFrom::Start(p) => self.off as isize + p as isize,
             std::io::SeekFrom::End(p) => self.data.len() as isize + p as isize,
@@ -227,12 +263,12 @@ impl Seek for BufferOutput {
         }
     }
 
-    fn rewind(&mut self) -> std::io::Result<()> {
+    fn rewind(&mut self) -> Result<()> {
         self.pos = self.off;
         Ok(())
     }
 
-    fn stream_position(&mut self) -> std::io::Result<u64> {
+    fn stream_position(&mut self) -> Result<u64> {
         Ok(self.pos as _)
     }
 }
