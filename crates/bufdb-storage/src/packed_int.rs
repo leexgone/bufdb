2b29ae95b2ffd0d8faf5d59009ee91f5fb42e9df
ljz <ljz@jiuqi.com.cn>
2023-05-09 21:21:34 +0800

    dev packed int

--- crates/bufdb-storage/src/packed_int.rs
+++ crates/bufdb-storage/src/packed_int.rs
@@ -4,33 +4,66 @@ use std::io::Result;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct PackedI32(pub i32);
 
+macro_rules! pack_write {
+    ($self: ident, $buf: expr) => {
+        {
+            let (mut val, negative) = if $self.0 < -119 {
+                (($self.0 + 119).abs(), true)
+            } else if $self.0 > 119 {
+                ($self.0 - 119, false)
+            } else {
+                $buf[0] = $self.0 as _;
+                return Ok(1);
+            };
+    
+            let mut len: usize = 0;
+            while val != 0 {
+                len = len + 1;
+                $buf[len] = val as u8;
+                val = val >> 8;
+            }
+    
+            $buf[0] = if negative { (- 119 - len as isize) as _ } else { (119 + len) as _ };
+    
+            Ok(len + 1)    
+        }
+    };
+}
+
+macro_rules! pack_read {
+    ($self: ident, $buf: expr, $t: ty) => {
+        {
+            let v = $buf[0] as i8;
+            let (negative, len) = if v < -119 {
+                (true, (v + 119).abs() as usize)
+            } else if v > 119 {
+                (false, (v - 119) as usize)
+            } else {
+                $self.0 = v as $t;
+                return Ok(1);
+            };
+    
+            let mut val = <$t>::default();
+            for i in (1..=len).rev() {
+                val = (val << 8) + $buf[i] as $t;
+            }
+    
+            $self.0 = if negative { - val - 119 } else { val + 119 };
+    
+            Ok(len + 1)
+        }
+    };
+}
+
 impl PackedI32 {
     pub const MAX_LENGETH: usize = 5;
 
     pub fn write(&self, buf: &mut [u8]) -> Result<usize> {
-        let (mut val, negative) = if self.0 < -119 {
-            (- self.0 - 119, true)
-        } else if self.0 > 119 {
-            (self.0 - 119, false)
-        } else {
-            buf[0] = self.0 as _;
-            return Ok(1);
-        };
-
-        let mut len: usize = 0;
-        while val & 0xFF != 0 && len <= 4 {
-            buf[len + 1] = val as u8;
-            len = len + 1;
-            val = val >> 8;
-        }
-
-        buf[0] = if negative { (- 119 - len as isize) as _ } else { (119 + len) as _ };
-
-        Ok(len)
+        pack_write!(self, buf)
     }
 
     pub fn read(&mut self, buf: &[u8]) -> Result<usize> {
-        todo!()
+        pack_read!(self, buf, i32)
     }
 }
 
@@ -59,11 +92,11 @@ impl PackedI64 {
     pub const MAX_LENGETH: usize = 9;
 
     pub fn write(&self, buf: &mut [u8]) -> Result<usize> {
-        todo!()
+        pack_write!(self, buf)
     }
 
     pub fn read(&mut self, buf: &[u8]) -> Result<usize> {
-        todo!()
+        pack_read!(self, buf, i64)
     }
 }
 
@@ -81,23 +114,77 @@ impl Into<i64> for PackedI64 {
 
 #[cfg(test)]
 mod tests {
+    use crate::packed_int::PackedI64;
+
+    use super::PackedI32;
+
+    macro_rules! check {
+        ($v: ident, $len: ident, $t: ty) => {
+            {
+                let mut buf = [0u8; <$t>::MAX_LENGETH];
+
+                let val = <$t>::from($v);
+                let size = val.write(&mut buf).unwrap();
+                if let Some(len) = $len {
+                    assert_eq!(len, size, "write len error");
+                }
+                let mut ret = <$t>::default();
+                let ret_len = ret.read(&buf).unwrap();
+                assert_eq!(size, ret_len, "read len error");
+                assert_eq!(val, ret, "read value error");
+            }            
+        };
+    }
+
+    fn check_i32(v: i32, len: Option<usize>) {
+        check!(v, len, PackedI32)
+    }
+
+    fn check_i64(v: i64, len: Option<usize>) {
+        check!(v, len, PackedI64)
+    }
+
     #[test]
-    fn test_byte() {
-        let v1 = -127i8;
-        let v2 = v1 as u8;
-        let v3 = v2 as i8;
+    fn test_packed_i32() {
+        check_i32(0, Some(1));
+        check_i32(1, Some(1));
+        check_i32(-1, Some(1));
+        check_i32(119, Some(1));
+        check_i32(-119, Some(1));
+        check_i32(120, Some(2));
+        check_i32(-120, Some(2));
+        check_i32(i32::MAX, Some(5));
+        check_i32(i32::MIN, Some(5));
+        check_i32(18, None);
+        check_i32(189, None);
+        check_i32(1834325324, None);
+        check_i32(770, None);
+        check_i32(-18, None);
+        check_i32(-189, None);
+        check_i32(-1834325324, None);
+        check_i32(-770, None);
+        check_i32(0xf010, None);
+    }
 
-        println!("{}, {}, {}", v1, v2, v3);
-
-        let v4 = -127i32;
-        let v5 = v4 as i8;
-        let v6 = v4 as u8;
-        let v7 = v5 as u8;
-        let v8 = v7 as i8;
-        println!("{}, {}, {}, {}, {}", v4, v5, v6, v7, v8);
-
-        let v9 = 0x10Fi16;
-        let v10 = v9 as i8;
-        println!("{}, {}", v9, v10);
+    #[test]
+    fn test_packed_i64() {
+        check_i64(0, Some(1));
+        check_i64(1, Some(1));
+        check_i64(-1, Some(1));
+        check_i64(119, Some(1));
+        check_i64(-119, Some(1));
+        check_i64(120, Some(2));
+        check_i64(-120, Some(2));
+        check_i64(i64::MAX, Some(9));
+        check_i64(i64::MIN, Some(9));
+        check_i64(18, None);
+        check_i64(189, None);
+        check_i64(1834325324, None);
+        check_i64(770, None);
+        check_i64(-18, None);
+        check_i64(-189, None);
+        check_i64(-1834325324, None);
+        check_i64(-770, None);
+        check_i64(0xf010, None);
     }
 }
